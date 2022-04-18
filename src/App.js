995a092e8dcdb8e351f0b079bbfdec46c39bb505
= <=>
2022-04-18 10:22:09 +0700

    update title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          vk iu ck
+          Luong Trang
         </p>
       </header>
     </div>
